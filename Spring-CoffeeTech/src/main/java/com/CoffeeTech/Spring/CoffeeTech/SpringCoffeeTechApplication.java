34ebca8d4f62bb135f13d6aa214ac2e4007cebea
Karen Molina <karen.molina99@soy.sena.edu.co>
2024-08-28 21:28:44 -0500

    Initial commit

---
+++ Spring-CoffeeTech/src/main/java/com/CoffeeTech/Spring/CoffeeTech/SpringCoffeeTechApplication.java
@@ -0,0 +1,13 @@
+package com.CoffeeTech.Spring.CoffeeTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoffeeTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoffeeTechApplication.class, args);
+	}
+
+}
---
+++ Spring-CoffeeTech/src/test/java/com/CoffeeTech/Spring/CoffeeTech/SpringCoffeeTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoffeeTech.Spring.CoffeeTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoffeeTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
